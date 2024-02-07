f8d888c063751cc9004081d9dfdbd0a054c4195b
Abdullaah-Ali <134156961+Abdullaah-Ali@users.noreply.github.com>
2024-02-07 13:37:05 +0500

    added the email function and the otp and user registration

--- routes/signup.js
+++ routes/signup.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose')
 const path = require('path');
 const jwt = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
+const otpGenerator = require('otp-generator')
+
 //to handle the json middleware is to be used
 
 
@@ -60,12 +63,52 @@ try {
     await newUser.save()
     res.status(201).json({ message: 'User registered successfully' });
 
-}
+    const otp = otpGenerator.generate(6, { upperCaseAlphabets: true, specialChars: true });
+    await sendEmail(email, 'Your OTP verification code is',  otp); // Replace '123456' with the actual OTP
+    
+
+
+    }
 catch(error){
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
 }
 
 });
+
+
+
+
+
+
+async function sendEmail(to,text,otp) {
+    try {
+        // Create a transporter using Gmail SMTP settings
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'ahere094@gmail.com',
+                pass: 'hpao tmpx hlyk jkkv'
+            }
+        });
+
+        // Define email content
+        let mailOptions = {
+            from: 'ahere094@gmail.com',
+            to: to,
+            subject: "OTP VERIFICATION EMAIL",
+            text: `${text} ${otp}`
+        };
+
+        // Send email
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Email sent:', info.response);
+    } catch (error) {
+        console.error('Error occurred:', error);
+    }
+}
+
+
+
 
 module.exports = router;
